89bcd0f767e8b1dcc252d3fa216b92fd1cb5c1da
Anastasiia <ambarnova94@gmail.com>
2025-04-27 12:00:54 +0200

    add lesson 2

---
+++ js/lesson-2.js
@@ -0,0 +1,92 @@
+// Створіть масив styles з елементами 'jazz' і 'blues'
+// Додайте до кінця масиву елемент 'rock-n-roll' за допомогою відповідного методу масивів
+// Знайдіть елемент 'blues' у масиві та замініть його на 'classic', використовуючи JavaScript-код
+
+// Напишіть функцію logItems(array), яка приймає масив як аргумент
+// і виводить у консоль кожен його елемент у форматі:
+// "<номер елемента> - <значення елемента>".
+// Використайте цикл for для перебору елементів масиву.
+// Нумерація елементів повинна починатися з 1 (а не з 0).
+
+const styles = ["jazz", "blues"];
+
+styles.push("rock-n-roll");
+
+const index = styles.indexOf("blues");
+if (index !== -1) {
+  styles[index] = "classic";
+}
+function logItems(array) {}
+
+console.log(styles);
+
+// Напишіть функцію checkLogin(array), яка:
+// Приймає масив логінів як аргумент.
+// Запитує ім'я користувача через prompt.
+// Перевіряє, чи є введене ім'я у переданому масиві.
+// Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
+// Якщо ім'я відсутнє – виводить повідомлення: "User not found".
+
+// const logins = ["Peter", "John", "Igor", "Sasha"];
+
+// Напишіть функцію caclculateAverage(),
+// яка приймає довільну кількість
+// аргументів і повертає їхнє середнє значення.
+// Додайте перевірку, що аргументи - це числа.
+
+function caclculateAverage() {
+  let sum = 0;
+  let avg = 0;
+
+  for (let index = 0; index < arguments.length; index++) {}
+}
+
+// Напишіть функцію, яка сумуватиме сусідні числа
+// і пушитиме їх в новий масив.
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
+// третє - з четвертим і так до кінця.
+// В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+
+function sumAdjacentNumbers(arr) {
+  const result = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    result.push(arr[i] + arr[i + 1]);
+  }
+
+  return result;
+}
+
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+console.log(sumAdjacentNumbers(someArr));
+
+// Напишіть функцію findSmallestNumber(numbers),
+// яка шукає найменше число в масиві.
+// Додайте перевірку, що функція отримує саме масив, і
+// якщо функція отримує масив - поверніть з функції найменше число,
+// в іншому випадку - поверніть 'Sory, it is not an array!'.
+
+// const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+
+// Напишіть функцію findLongestWord(string), яка
+// приймає довільний рядок, що складається лише зі слів, розділених
+// пробілами (параметр string), і повертатиме найдовше слово у реченні.
+
+// Скористайтесь цим прикладом виклику функції для перевірки її роботи:
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+function findLongestWord(string) {
+  let longestWord = "";
+  const words = string.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord("London is the capital of Great Britain"));
